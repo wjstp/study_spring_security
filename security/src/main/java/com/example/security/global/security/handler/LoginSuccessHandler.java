51f4e10b3f04221a783d8ae896dd355b898bcfaa
wjstp <wjstp14@gmail.com>
2024-04-18 15:03:04 +0900

    feat: refresh token

--- security/src/main/java/com/example/security/global/security/handler/LoginSuccessHandler.java
+++ security/src/main/java/com/example/security/global/security/handler/LoginSuccessHandler.java
@@ -3,6 +3,7 @@ package com.example.security.global.security.handler;
 import com.example.security.global.security.dto.CustomUserDetailsDTO;
 import com.example.security.global.security.filter.JwtUtil;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +22,29 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         CustomUserDetailsDTO customUserDetails = (CustomUserDetailsDTO) authentication.getPrincipal();
-        // token 생성
-        String token = jwtUtil.generateAccessToken(customUserDetails);
-        response.addHeader("Authorization", "Bearer " + token);
+        // access token 생성
+        String accessToken = jwtUtil.generateAccessToken(customUserDetails);
+        response.addHeader("Authorization", "Bearer " + accessToken);
+        // refresh token 발급
+        String refreshToken = jwtUtil.generateRefreshToken(accessToken, customUserDetails);
+        Cookie cookie = createCookie(refreshToken);
+        response.addCookie(cookie);
+
+        //로그인 성공 메세지
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("{\"message\":\"로그인되었습니다.\"}");
+    }
+
+
+    public Cookie createCookie(String refreshToken) {
+        String cookieName = "Refresh-Token";
+        Cookie cookie = new Cookie(cookieName, refreshToken);
+        // 쿠키 속성 설정
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/auth/refresh");
+        cookie.setMaxAge(15&50*60*24);
+        return cookie;
     }
 }
